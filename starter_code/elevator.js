3e99dceb6e65aa11dbcd819f112ea5dca2324dab
Anton Virtanen <avirtanen@hotmail.fr>
2017-02-07 09:43:10 +0100

    not done

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,91 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
+  constructor(floor, requests){
+    this.floor      = floor;
     this.MAXFLOOR   = 10;
     this.requests   = [];
+    if (this.requests[0] < this.floor){
+      this.direction = "Down";
+    } else if (this.requests[0] > this.floor){
+      this.direction = "Up";
+    } else {
+      this.direction = "none";}
   }
 
-  start() { }
-  stop() { }
-  update() { }
+  start() {
+    setInterval(()=> this.update(), 1000);
+  }
+
+  stop() { clearInterval(this.start);}
+
+  update() {
+    // this.requests[0];
+    this.log();}
+
   _passengersEnter() { }
   _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+
+  floorUp(floor){
+    setInterval(()=> this.floor +=1,1000);
+    // if (this.floor == this.MAXFLOOR){
+    //
+    // }
+
 }
+
+  floorDown() {
+    if (this.floor <= 0){
+      return this.floor;
+    } else if (this.floor > 0){
+      this.direction ="Down";
+      this.floor -= 1;
+    }
+  }
+  call(person) {
+    return this.requests.push(person);
+   }
+
+  log() {
+    console.log("Direction: " + this.direction + " | Floor:"+ this.floor);
+  }
+}
+
+var floor = new Elevator(2,3);
+
+// console.log(floor.log());
+// console.log(floor.floorUp());
+// console.log(floor.log());
+// console.log(floor.floorUp());
+// console.log(floor.log());
+// console.log(floor.floorUp());
+// console.log(floor.log());
+// console.log(floor.floorUp());
+// console.log(floor.log());
+// console.log(floor.floorUp());
+// console.log(floor.log());
+// console.log(floor.floorUp());
+// console.log(floor.log());
+// console.log(floor.floorUp());
+// console.log(floor.log());
+// console.log(floor.floorDown());
+// console.log(floor.log());
+// console.log(floor.floorDown());
+// console.log(floor.log());
+// console.log(floor.floorDown());
+// console.log(floor.log());
+// console.log(floor.floorDown());
+// console.log(floor.log());
+// console.log(floor.floorDown());
+// console.log(floor.log());
+// console.log(floor.floorDown());
+// console.log(floor.log());
+// console.log(floor.floorDown());
+// console.log(floor.log());
+// console.log(floor.floorDown());
+// console.log(floor.log());
+// console.log(floor.floorDown());
+// console.log(floor.log());
+// console.log(floor.floorDown());
+// console.log(floor.log());
+
 
 module.exports = Elevator;
--- starter_code/index.js
+++ starter_code/index.js
@@ -1,1 +1,11 @@
 const Elevator = require('./elevator.js');
+const Person = require('./person.js');
+
+
+var elevator = new Elevator(2,3);
+var pedro = new Person("Pedro", 1, 2);
+var julia = new Person("Julia", 2, 3);
+elevator.call(pedro);
+elevator.call(julia);
+elevator.floorUp();
+// console.log(elevator.requests[0]);
